c0a4c2dd668a7387fcc8c40a4ea0f400630f7dc1
NAT1804 <51477539+NAT1804@users.noreply.github.com>
2022-11-30 06:29:31 +0700

    5 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, VERSION } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  my_menu = of({
+    main1: ['sub1', 'sub2'],
+    main2: ['sub1', 'sub2', 'sub3'],
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppMaterialModule } from './app-material.module';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PortalModule } from '@angular/cdk/portal';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppMaterialModule,
+    BrowserAnimationsModule,
+    PortalModule,
+  ],
+  declarations: [AppComponent, HelloComponent, MenuComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
